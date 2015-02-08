35a47091a5729402a9d19b73f07644e1bbaf82d6
Daniel Burkard <danielburakrd@me.com>
2015-02-08 12:22:54 +0100

    README and Comments

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # udacity_fullstack_project2
 udacity Full Stack Web Developer Nanodegree Project 2
+
+##Files
+* tournament.py -- implementation of a Swiss-system tournament
+* tournament.sql -- table definitions for the tournament project.
+* tournament_test.py -- Test cases for tournament.py
+
+##Usage
+```bash
+vagrant@vagrant-ubuntu-trusty-32:/vagrant/tournament$ psql tournament < tournament.sql 
+```
+```bash
+vagrant@vagrant-ubuntu-trusty-32:/vagrant/tournament$ python tournament_test.py 
+1. Old matches can be deleted.
+2. Player records can be deleted.
+3. After deleting, countPlayers() returns zero.
+4. After registering a player, countPlayers() returns 1.
+5. Players can be registered and deleted.
+6. Newly registered players appear in the standings with no matches.
+7. After a match, players have updated standings.
+8. After one match, players with one win are paired.
+Success!  All tests pass!
+
+```
--- tournament.sql
+++ tournament.sql
@@ -5,17 +5,22 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+
+-- DROP old Tables
 -- DROP VIEW IF EXISTS OMW;
 DROP VIEW IF EXISTS Standings;
 DROP View IF EXISTS Count;
 DROP VIEW IF EXISTS Wins;
-
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Players;
+
+-- Players Table
 CREATE TABLE Players (
 	id SERIAL primary key,
 	name varchar(255)
 );
+
+-- Matches Table
 CREATE TABLE Matches (
 	id SERIAL primary key,
 	player int references Players(id),
@@ -23,22 +28,29 @@ CREATE TABLE Matches (
 	result int
 );
 
+-- Wins View shows number of wins for each Player
 CREATE VIEW Wins AS
 	SELECT Players.id, COUNT(Matches.opponent) AS n 
 	FROM Players
 	LEFT JOIN (SELECT * FROM Matches WHERE result>0) as Matches
 	ON Players.id = Matches.player
 	GROUP BY Players.id;
+
+-- Count View shows number of matches for each Player
 CREATE VIEW Count AS
 	SELECT Players.id, Count(Matches.opponent) AS n 
 	FROM Players
 	LEFT JOIN Matches
 	ON Players.id = Matches.player
 	GROUP BY Players.id;
+
+-- Standings View shows number of wins and matches for each Player
 CREATE VIEW Standings AS 
 	SELECT Players.id,Players.name,Wins.n as wins,Count.n as matches 
 	FROM Players,Count,Wins
 	WHERE Players.id = Wins.id and Wins.id = Count.id;
+
+-- Standings OMW shows number of "Opponent Match Wins" for each Player
 -- CREATE VIEW OMW AS
 -- 	SELECT Players.id, sum(Standings.wins) as n
 -- 	FROM Players,Standings,Matches
